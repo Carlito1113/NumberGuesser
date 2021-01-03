9f824fd25a7684c2a88cf4cc12de93ef0900088a
Carlito Algarin <carlito1113@yahoo.com>
2021-01-03 16:53:34 -0500

    final commit, the application now has a play again button upon the game being over from either winning or losing; a random number is selected each time

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 // Game values
 let min = 1,
   max = 10,
-  winningNum = 2,
+  winningNum = getRandomNum(min, max),
   guessesLeft = 3;
 
 // UI Elements
 const game = document.querySelector("#game"),
-  minNum = document.querySelector(".min-num"),
-  maxNum = document.querySelector(".max-num"),
-  guessBtn = document.querySelector("#guess-btn"),
-  guessInput = document.querySelector("#guess-input"),
-  message = document.querySelector(".message");
+      minNum = document.querySelector(".min-num"),
+      maxNum = document.querySelector(".max-num"),
+      guessBtn = document.querySelector("#guess-btn"),
+      guessInput = document.querySelector("#guess-input"),
+      message = document.querySelector(".message");
 
 // Assign UI min and max
 minNum.textContent = min;
 maxNum.textContent = max;
+
+// Play again event listener
+game.addEventListener('mousedown', function(e) {
+  if(e.target.className === 'play-again') {
+    window.location.reload();
+  }
+});
 
 // Listen for guess
 guessBtn.addEventListener("click", function () {
@@ -68,6 +75,15 @@ guessInput.style.borderColor = color;
 message.style.color = color;
 // Set message
 setMessage(msg);
+
+// Play Again?
+guessBtn.value = 'Play Again';
+guessBtn.className += 'play-again';
+}
+
+// Get winning number
+function getRandomNum(min, max){
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 // Set message
